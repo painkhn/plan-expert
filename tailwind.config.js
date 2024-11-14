f206f41e7a4456eca5fce904a3197f75fc312b16
painkhn <1fotice@gmail.com>
2024-11-14 22:44:11 +0800

    project upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "*.html",
+    "./pages/*.html",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')({
+        charts: true,
+    }),
+  ],
+  darkMode: "class"
+}
